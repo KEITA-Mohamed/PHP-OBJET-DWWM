204423de62de8d09d28f032e9c0c5e4075b8cc69
KEITA-Mohamed <mohamed.keita99@gmail.com>
2024-02-01 15:30:05 +0100

    01/02/2024 aprem

---
+++ Config/parametre.php
@@ -0,0 +1,11 @@
+<?php
+const HOST='127.0.0.1';
+const DBNAME='dwwm';
+const USER='admin';
+const PASSWORD='4321';
+// On peut aussi definir une constante par define
+// define("HOST","127.0.0.1");
+
+
+
+?>
